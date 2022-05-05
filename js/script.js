1d45f5d52500d09c9e5bc39b6216c36c0dc0da28
)- <a-@-s-MacBook-Pro.local>
2022-05-05 17:02:39 -0400

    added selector variables

--- js/script.js
+++ js/script.js
@@ -6,7 +6,18 @@
 // 1.jQuery $.ajax() function
 // 2. the browser fetch() function
 // 3.using the Axios http client libary
+
+//Variables
 const URL = "http://www.omdbapi.com/?apikey=62612635&t=Die+Hard"
+
+// Element References
+
+//Event Listeners
+const $title = $('#title');
+const $year = $('#year');
+const $rating = $('#rating');
+
+//FUNCTIONS
 $.ajax(URL).then(function(data) {
     console.log('movie data is ready')
     console.log(data)
